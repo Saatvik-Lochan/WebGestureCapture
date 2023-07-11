b407cec86d22c1ffba3ca772971c78b90920092b
Saatvik Lochan <saatviklochan@gmail.com>
2023-07-11 13:34:00 +0100

    add getHandDataAsArray

--- src/hand_capture.ts
+++ src/hand_capture.ts
@@ -2,6 +2,13 @@ import { Clock, XRHandSpace, WebGLRenderer } from "three";
 import { frameListeners } from "./main";
 
 function getHandDataAsString(renderer: WebGLRenderer, clock: Clock) {
+	const arrayData = getHandDataAsArray(renderer, clock);
+	const stringData = arrayData.map(element => element.toFixed(6)).join(",")
+
+	return stringData;
+}
+
+function getHandDataAsArray(renderer: WebGLRenderer, clock: Clock) {
 	const hand0 = renderer.xr.getHand(0);
 	const hand1 = renderer.xr.getHand(1);
 
@@ -42,22 +49,42 @@ function getHandDataAsString(renderer: WebGLRenderer, clock: Clock) {
 		return `${poseArray.join(',')},`;
 	}
 
-	function getHandAsString(handObj: XRHandSpace) {
-		let out_str = '';
+	function getJointAsArr(handObj: XRHandSpace, jointName: string) {
+		const poseArray = 
+			[...handObj.joints[jointName].position.toArray(),
+			 ...handObj.joints[jointName].quaternion.toArray()];
+
+		return poseArray;
+	}
+
+	function getHandAsArray(handObj: XRHandSpace) {
+		let out_arr = [];
 		for (let i = 0; i <= 24; i++) {
-			out_str += getJointAsString(handObj, indexToName[i]);
+			out_arr = [...out_arr, ...getJointAsArr(handObj, indexToName[i])];
 		}
-		return out_str
+		return out_arr
 	}
 
 	const captureStartTime = clock.getElapsedTime();
-	const capturedData = getHandAsString(hand0) + getHandAsString(hand1);
+	const capturedData = [...getHandAsArray(hand0), ...getHandAsArray(hand1)];
 	const captureEndTime = clock.getElapsedTime();
 
-	const finalData = `${capturedData}${captureStartTime},${captureEndTime}\n`
+	const finalData = [...capturedData, captureStartTime, captureEndTime];
 
 	return finalData;
+}
 
+async function streamHandCapture(durationMs: number, renderer: WebGLRenderer) {
+	const clock = new Clock(true);
+	const capturedData = [];
+	
+	frameListeners[0] = 
+		() => capturedData.push(getHandDataAsString(renderer, clock));
+
+	await new Promise(resolve => setTimeout(resolve, durationMs));
+	delete frameListeners[0];
+
+	return capturedData.join("");
 }
 
 async function captureHandSequence(durationMs: number, renderer: WebGLRenderer) {
--- src/http_handler.ts
+++ src/http_handler.ts
@@ -8,7 +8,6 @@ function formatHandData(handData: any, title: string) {
     }
 }
 
-
 // handles sending the data with a POST request
 async function sendData(data: object, route = "") {
     return await fetch(`${backend_url}/${route}`, {
