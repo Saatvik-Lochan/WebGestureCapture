d470e335c7992bc75a0d6196662a1a88de950d45
Saatvik Lochan <saatviklochan@gmail.com>
2023-08-02 10:25:53 +0100

    add nested directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig ({
+  base: '',
+  root: resolve(__dirname, 'src'),
   plugins: [
     basicSsl()
-  ]
-}
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        demonstration: resolve(__dirname, 'src/demonstration/index.html'),
+      },
+    },
+  },
+})
